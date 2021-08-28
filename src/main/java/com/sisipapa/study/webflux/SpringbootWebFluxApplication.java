9169bcb3227cc5d7f0fcc4983274392555f9c28f
KKH <coolguy239@naver.com>
2021-08-29 00:28:18 +0900

    Initial commit

---
+++ src/main/java/com/sisipapa/study/webflux/SpringbootWebFluxApplication.java
@@ -0,0 +1,13 @@
+package com.sisipapa.study.webflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWebFluxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootWebFluxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sisipapa/study/webflux/SpringbootWebFluxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sisipapa.study.webflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootWebFluxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
